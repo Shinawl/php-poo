94d1d5621e125a8f4d714065b1e833c6288cbc66
tom.perret51@gmail.com <tom.perret51@gmail.com>
2022-02-01 10:45:21 +0100

    test

--- Exo-2/ToDoList.php
+++ Exo-2/ToDoList.php
@@ -1,3 +1,45 @@
 <?php
 
-//
+
+class Todo {
+
+    public ?DateTime $completed_at = null;
+
+    public function __construct(
+        public string $title,
+        public string $description,
+    )
+    {
+    }
+
+    public function setCompleted(): self
+    {
+        $this->completed_at = new DateTime();
+
+        return $this;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this->completed_at !== null;
+    }
+}
+
+class ToDoList
+{
+
+}
+
+$todo1 = new Todo(
+    title: 'Ma todo',
+    description: 'Ma description',
+);
+
+var_dump($todo1->isCompleted());
+
+$todo1->setCompleted();
+
+var_dump($todo1->isCompleted());
+
+
+
